563b84e3defd3db978bc359ccb4b07f275996477
Umesh <umesh.timalsina@vanderbilt.edu>
2023-09-04 18:14:35 -0500

    Add FastAPI video node. Closes #1

--- nursing_simulation/__init__.py
+++ nursing_simulation/__init__.py
@@ -2,4 +2,9 @@ from typing import Any, Dict
 
 
 def register_nodes_metadata() -> Dict[str, Any]:
-    return {}
+    return {
+        "nodes": [
+            "nursing_simulation.fastapi_server_node:FastAPIVideoNode",
+        ],
+        "description": "Nodes from the nursing simulation package",
+    }
